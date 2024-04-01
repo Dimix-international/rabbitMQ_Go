e9376d0ea09aaea53df47b72611ec467dd93f9c0
Dimix-international <dima.dimix94@mail.ru>
2024-04-01 16:51:53 +0300

    main: refactor

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"time"
 
-	rabbitmq "github.com/Dimix-international/rabbitMQ_Go/internal/rabbitMq"
+	"github.com/Dimix-international/rabbitMQ_Go/internal/rabbitMq"
 )
 
 func main() {
-	clientRabbit, err := rabbitmq.NewRabbitClient("dima", "qwerty", "localhost:5672", "customers")
+	clientRabbit, err := rabbitMq.NewRabbitClient("dima", "qwerty", "localhost:5672", "customers")
 	if err != nil {
 		log.Panicf("%s", err)
 	}
